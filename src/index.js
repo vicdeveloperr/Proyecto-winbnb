ee4034ee33b50cceca02d04d36bcb9867cd0055a
vickyflow <vickyflow6@gmail.com>
2022-10-30 09:31:22 -0300

    ready

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 import reportWebVitals from './reportWebVitals';
-=======
->>>>>>> 4d19897d304d1c77b4a4608c8185dafbcba1c4ab
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App />
   </React.StrictMode>
 );
@@ -19,10 +15,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-=======
-    <App propiedadesFiltroHuespedes={10} />
-  </React.StrictMode>
-);
+    <Reac.StrictMode>
+        <App propiedadesFiltroHuespedes={10} />
+    </React.StrictMode>
 
 export default root;
->>>>>>> 4d19897d304d1c77b4a4608c8185dafbcba1c4ab
